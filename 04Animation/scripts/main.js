d215f63bce3afd8af227dda3bdd50b5fee905e0d
iiZenos <108242282+iiZenos@users.noreply.github.com>
2022-06-27 02:48:46 +0200

    Add files via upload

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,11 +1,13 @@
 
 
 // changing individual properties with code and using setInterval
-var rotationSpeed = 0.01;
+var rotationSpeed = 0.03;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
 	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
 }
 
